3b489baf6cc1601790fedd1d4bc0b37d4a39e99a
Bo Li <drluodian@gmail.com>
2025-09-07 14:53:12 +0000

    feat(train): Add MiniGrid support and document all Gymnasium environments

--- easy_install.sh
+++ easy_install.sh
@@ -2,6 +2,7 @@ uv venv --python=3.13
 source .venv/bin/activate
 
 uv pip install -e .
+uv pip install minigrid
 uv pip install 'stable-baselines3[extra]'
 uv pip install "gymnasium[box2d]"
 uv pip install "gymnasium[atari,accept-rom-license]" shimmy
